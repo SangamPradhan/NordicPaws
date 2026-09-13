50d2ec438d1235e50da0d6bda7a3683a5d78620b
SangamPradhan <pradhansangam160@gmail.com>
2026-09-13 07:57:19 +0545

    addewd new fragments

---
+++ app/src/main/java/com/sangampradhan/nordicpaws/fragments/PlacesFragment.java
@@ -0,0 +1,26 @@
+package com.sangampradhan.nordicpaws.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.sangampradhan.nordicpaws.R;
+
+public class PlacesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_places, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getActivity() instanceof com.sangampradhan.nordicpaws.MainActivity) {
+            ((com.sangampradhan.nordicpaws.MainActivity) getActivity()).setTopBarTitle("PLACES");
+        }
+    }
+}
