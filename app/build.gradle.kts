0799785c5c64b581628bf7a96498c1022512053e
SangamPradhan <pradhansangam160@gmail.com>
2026-09-14 09:45:18 +0545

    added screens for places and pet profiles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
+    implementation(libs.viewpager2)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
