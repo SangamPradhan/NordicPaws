349661ef7b0897b1853026aa37b81e4d8152bd22
SangamPradhan <pradhansangam160@gmail.com>
2026-09-23 20:59:24 +0545

    added screen and activity for expense tracker

---
+++ app/src/main/java/com/sangampradhan/nordicpaws/fragments/ExpenseTrackerFragment.java
@@ -0,0 +1,42 @@
+package com.sangampradhan.nordicpaws.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.sangampradhan.nordicpaws.MainActivity;
+import com.sangampradhan.nordicpaws.R;
+
+public class ExpenseTrackerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_expense_tracker, container, false);
+
+        ImageButton btnBack = view.findViewById(R.id.btnBackExpenses);
+        if (btnBack != null) {
+            btnBack.setOnClickListener(v -> {
+                if (getActivity() != null) {
+                    getActivity().onBackPressed();
+                }
+            });
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getActivity() instanceof MainActivity) {
+            ((MainActivity) getActivity()).setTopBarTitle("EXPENSES");
+        }
+    }
+}
